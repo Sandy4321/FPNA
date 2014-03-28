f9abccec9db911a9f154edea2aefe7eae4ac1871
Ryan <Ryan@Kinnear.ca>
2014-03-28 14:04:07 -0600

    Implemented buildnetwork function.  Lots of errors to deal with.

--- Brain/brain.py
+++ Brain/brain.py
@@ -5,7 +5,9 @@
 
 #This is currently a sequential implementation
 
-from tissue import InputNode, HiddenNode, OutputNode, Link, Activator
+from tissue import InputNode, HiddenNode, Link, Activator
+import warnings
+import math
 
 #------------------------------------------------------------------------------
 class Brain():
@@ -37,22 +39,25 @@ class Brain():
     '''
     assert isinstance(N, list), 'N must be a list'
     assert all([isinstance(t, (tuple,list)) for t in N])
-    assert all([len(t) == 2 for t in N])
-    assert all([isinstance(x[0], float) or x[0] == None
+    assert all([len(t) == 3 for t in N])
+    assert all([(isinstance(x[0], float) or x[0] == None)
                 and isinstance(x[1], int)
-                and isinstance(x[2], Activator) for x in N])
+                and (isinstance(x[2], Activator)
+                or x[2] == None) for x in N])
 
     assert all([isinstance(l, list) for l in E])
     n = len(N)
     for l in E:
-      assert all([isinstance(x[0], float) and isinstance(x[1], float)
+      assert all([(x == None or x == ())
+                  or (isinstance(x[0], float) and isinstance(x[1], float)
                   and isinstance(x[2], (bool)) and isinstance(x[3], bool)
-                  and isinstance(x[4], list) for x in l])
+                  and isinstance(x[4], list))
+                  for x in l])
       assert all([isinstance(x, int) and x>=0 and x<=n  for x[4] in l])
 
     assert all([len(l) == n for l in E])
     assert name == None or isinstance(name, str), 'name should be a string'
-#    (W, T, r, S, [R])
+
     self.n = n #The number of nodes
     self.E = E
     self.A = A
@@ -70,8 +75,43 @@ class Brain():
   #----------------------------------------------------------------------------  
   def buildNetwork(self):
     '''
-    Constructs a network based off of N, E, A.
+    Constructs a network based off of N, and E.
     '''
+    #N: (theta, a, A)
+    for i in range(len(self.N)):
+      if(self.N[i][0] == None): #Input node
+        assert (self.N[i][2] == None)
+        self.N[i] = InputNode(n[i][1],'inN' + str(i))
+      else:
+        self.N[i] = Node(self.N[i][2], self.N[i][0], self.N[i][1],
+                         'N' + str(i))
+
+    E = [[]]
+    i = 0
+    for row in self.E:
+      j = 0
+      for l in row:
+        if l == None or l == ():
+          E[i].append(None)
+        else:
+          E[i].append(Link(l[0], l[1], 'L' + str(i) + str(j)))
+        j += 1
+      i += 1
+
+    i = 0
+    for row in self.E:
+      j = 0
+      for l in row:
+        if l == None or l == ():
+          pass
+        else:
+          self.N[j].bind(E[i][j], l[4], l[3])
+          if l[2]:
+            self.N[i].createconnection(E[i][j])
+        j += 1
+      i += 1
+
+    self.E = E
     return
 
   #----------------------------------------------------------------------------  
@@ -79,4 +119,24 @@ class Brain():
     return
 
 #==============================================================================
-l = Link(1.0, 2.0)
+def i(x, xp):
+  return x + xp
+
+def f(x):
+  math.tanh(x)
+
+A = Activator(i, f, 'act')
+
+#(theta, a, A)
+#(W, T, r, S, [R])
+#W = i*j
+#T = i + j
+N = [(None, 2, None), (None, 1, None), (2.1, 3, A), (-1.9, 2, A), (0.0, 2, A)]
+E = [[(), (), (3,4,0,True,[4]), (), ()],
+     [(), (), (6,5,True,True,[]), (8,6,True,True,[]), ()],
+     [(), (), (), (12,7,True,False,[]), (15,8,True,True,[])],
+     [(), (), (), (), (20,9,True,True,[])],
+     [(), (), (15,8,True,False,[]), (), ()]]
+
+
+B = Brain(N, E, 'I\m a Brain!')

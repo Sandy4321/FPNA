0caaaa476800c5494b9b296129f3d0caac46006e
Ryan <Ryan@Kinnear.ca>
2014-03-28 09:46:09 -0600

    vLinks no longer need to go through nodes.  Links can connect directly to other Links.

--- Brain/tissue.py
+++ Brain/tissue.py
@@ -71,7 +71,7 @@ class HiddenNode():
     assert all([isinstance(o, Link) for o in outputs]), 'ouputs must be Links'
 
     self.outputConnections = [] #[L1, L2, ...]
-    self.Queue = {} #{Link : ([floats], [Links], r) ...}
+    self.Queue = {} #{L1 : [], L2 : [], ...}
     self.activator = activator
     self.name = name
     self.theta = theta
@@ -95,18 +95,15 @@ class HiddenNode():
     '''
     for Lin in self.Queue: #Iterate through all queues
       while self.Queue[Lin][0] != []:
-        x = self.Queue[Lin][0].pop(0) #Take the first element from the queue
-        for Lout in self.Queue[Lin][1]:
-          Lout.push(x) #Send values to each Queue's virtual links
-        if self.Queue[Lin][2]: #If r == 1 apply the activator
-          self.x = self.activator.iterate(x) #call iterate function
-          self.c += 1 #increment the counter
-          if self.c == self.a: #If the counter has reached it's max vlue
-            self.c = 0 #reset the counter
-            y = self.activator.activate(x) #calculate the activation of x
-            self.x = theta #reset x
-            for Lout in self.outputConnections:
-              Lout.push(y) #output the activation to all output connections
+        x = self.Queue[Lin].pop(0) #Take the first element from the queue
+        self.x = self.activator.iterate(self.x, x) #call iterate function
+        self.c += 1 #increment the counter
+        if self.c == self.a: #If the counter has reached it's max vlue
+          self.c = 0 #reset the counter
+          y = self.activator.activate(x) #calculate the activation of x
+          self.x = theta #reset x
+          for Lout in self.outputConnections:
+            Lout.push(y) #output the activation to all output connections
     return
 
   #----------------------------------------------------------------------------  
@@ -117,25 +114,19 @@ class HiddenNode():
     assert isinstance(x, float), 'x must be a float'
     assert isinstance(L, Link), 'L must be a Link'
     assert L in self.Queue, 'L must be in the Queue'
-    self.Queue[x][0].append(x)
+    self.Queue[x].append(x)
     return
 
   #----------------------------------------------------------------------------  
-  def bind(self, L, vLinks = [], r = False):
+  def bind(self, L):
     '''
     The Link L binds itself to an input of this Node.  An attempt to bind 
-    more than once is considered an error.  If r is True, then inputs from
-    L will go to the activator.
+    more than once is considered an error.
     '''
     assert isinstance(L, Link), 'L must be a Link'
-    assert isinstance(vLinks, list), 'vLinks must be a list'
-    assert all([isinstance(l, Link) for l in vLinks]), 'vLinks must be Links'
-    assert isinstance(r, bool), 'r must be a bool'
     assert L not in self.Queue, 'Attempt by ' + str(L) + ' to bind twice'
-    if L not in self.outputConnections:
-      for l in vLinks:
-        l.bind(self) #Bind to each of the links
-    self.Queue[L] = [[], vLinks, r]
+    assert L not in self.outputConnections, 'Attempt to create loop'
+    self.Queue[L] = []
     return
 
   #----------------------------------------------------------------------------  
@@ -144,10 +135,10 @@ class HiddenNode():
     Creates a connection from this Node to a Link.  All outputs from the
     activator will be sent to this link.
     '''
-    assert isinstance(L_out, Link) or L_out == None, 'L_out must be a Link'
-    assert L_out not in self.outputConnections, 'Attempt to make conn twice'
-    if L_out not in self.Queue:
-      L_out.bind(self) #Bind to the input of the Link
+    assert isinstance(L_out, Link), 'L_out must be a Link'
+    assert L_out not in self.outputConnections, 'Attempt to create conn twice'
+    assert L_out not in self.Queue, 'Attempt to create conn twice'
+    L_out.bind(self) #Bind to the input of the Link
     self.outputConnections.append(L_out)
     return
 
@@ -159,15 +150,15 @@ class Link():
   def __init__(self, W, T, name = None):
     '''
     W: Weight for affine transform W*x + T
-    T: Weight for affine transfor W*x + t
+    T: Weight for affine transfor W*x + T
     name: The name of this particular link
     '''
     assert isinstance(W, (float)), 'W must be a float'
     assert isinstance(T, (float)), 'T must be a float'
     assert isinstance(name, str) or name == None, 'name must be a string'
     
-    self.inputNode = None #The node bound to this link
-    self.outputNode = None #The node this link is bound to
+    self.inputConn = None #The node or link bound to this link
+    self.outputConn = None #The node or link this link is bound to
 
     self.W = W
     self.T = T
@@ -185,47 +176,48 @@ class Link():
   def activate(self):
     '''
     Applies the affine transform Wx + T to every value in the Queue and then
-    pushes the values to the outputNode.
+    pushes the values to the outputConn.
     '''
     while self.Queue != []:
       y = self.W*self.Queue.pop(0) + self.T
-      self.outputNode.push(y)
+      self.outputConn.push(y)
     return
 
   #----------------------------------------------------------------------------  
-  def push(self, N, x):
+  def push(self, R, x):
     '''
-    The Node N pushes the float x to the queue of this Link.  The Node N
-    must be bound to the Link and the register must be empty (contain None).
+    The resource R pushes the float x to the queue of this Link.  R must be 
+    bound to the Link.
     '''
     assert isinstance(x, float), 'x must be a float'
-    assert isinstance(N, InputNode, HiddenNode), 'N must be a Node'
-    assert N == self.inputNode, str(N) + 'must be bound to this node'
+    assert isinstance(N, InputNode, HiddenNode, Link), 'R is not a valid type'
+    assert R == self.inputConn, str(R) + 'must be bound to this node'
     self.Queue.append(x)
     return
                         
   #----------------------------------------------------------------------------  
   def bind(self, N):
     '''
-    The node N binds itself to the input of this Link.  The Link may only have
-    one node bound to it at a time, and re-binding is considered an error.
+    The resource R binds itself to the input of this Link.  The Link may only
+    have one resource bound to it at a time, and re-binding is considered an
+    error.
     '''
-    assert isinstance(N, (InputNode, HiddenNode)), 'N must be a node'
-    assert self.inputNode == None, 'Attempt to re-bind to link'
-    self.inputNode = N
+    assert isinstance(N, (InputNode, HiddenNode, Link)), 'R not valid type'
+    assert self.inputConn == None, 'Attempt to re-bind to link'
+    self.inputConn = N
     return
 
   #----------------------------------------------------------------------------  
-  def setOutputNode(self, N):
+  def createConnection(self, R):
     '''
-    Sets the Node N to this Link's output node.  That is, the node to which
-    this link writes it's output.  Attempting to change this connection
-    is treated as an error.
+    Sets the resource R to this Link's outputConn.  That is, the resource
+    to which this link writes it's output.  Attempting to change this 
+    connection is treated as an error.
     '''
-    assert isinstance(N, HiddenNode), 'N must be a Node'
+    assert isinstance(R, (HiddenNode, Link)), 'R must be a Node'
     assert self.outputNode == None
-    N.bind(self) #Bind to the node
-    self.outputNode = N
+    R.bind(self) #Bind to the node
+    self.outputNode = R
     return
 
 #------------------------------------------------------------------------------
@@ -269,10 +261,10 @@ class Activator():
     return self.f(x)
 
   #----------------------------------------------------------------------------  
-  def iterate(self, x):
+  def iterate(self, x, xp):
     '''
     Calls the iterator function i on x and returns i's return value.
     No error handling happens here.  It should be done inside the Node 
     that contains this activator
     '''
-    return self.i(x)
+    return self.i(x, xp)
